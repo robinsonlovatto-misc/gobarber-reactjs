7828d7c4c98b9d9a30eaba2e8d3099d88a463710
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-07-25 16:36:38 +0100

    Signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
     <>
-      <SignIn />
+      <SignUp />
       <GlobalStyle />
     </>
 
