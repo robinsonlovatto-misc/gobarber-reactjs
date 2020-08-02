418f1faaf4cef4f6d428e4a67d836e7bbfa248fa
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-08-02 21:06:02 +0100

    adding and removing Toasts messages

--- src/hooks/toast.tsx
+++ src/hooks/toast.tsx
@@ -1,25 +1,54 @@
-import React, { createContext, useCallback, useContext } from 'react';
+import React, { createContext, useCallback, useContext, useState } from 'react';
+import { uuid } from 'uuidv4';
 import ToastContainer from '../componentes/ToastContainer';
 
+export interface ToastMessage {
+  // id is necessary because react needs an id for the key property inside a map
+  id: string;
+  type?: 'success' | 'error' | 'info';
+  title: string;
+  description?: string;
+}
+
 interface ToastContextData {
-  addToast(): void;
-  removeToast(): void;
+  addToast(message: Omit<ToastMessage, 'id'>): void;
+  removeToast(id: string): void;
 }
 
 const ToastContext = createContext<ToastContextData>({} as ToastContextData);
 
 const ToastProvider: React.FC = ({ children }) => {
-  const addToast = useCallback(() => {
-    console.log('addToast');
-  }, []);
-  const removeToast = useCallback(() => {
-    console.log('removeToast');
+  const [messages, setMessages] = useState<ToastMessage[]>([]);
+
+  // omit - all the properties of the interface ToastMessage except for id
+  // const addToast = useCallback((message: Omit<ToastMessage,'id'>) => {
+  const addToast = useCallback(
+    ({ type, title, description }: Omit<ToastMessage, 'id'>) => {
+      const id = uuid();
+
+      const toast = {
+        id,
+        type,
+        title,
+        description,
+      };
+
+      // setMessages([...messages, toast]);
+      setMessages(oldMessages => [...oldMessages, toast]);
+    },
+    [],
+  );
+
+  const removeToast = useCallback((id: string) => {
+    setMessages(oldMessages =>
+      oldMessages.filter(message => message.id !== id),
+    );
   }, []);
 
   return (
     <ToastContext.Provider value={{ addToast, removeToast }}>
       {children}
-      <ToastContainer />
+      <ToastContainer messages={messages} />
     </ToastContext.Provider>
   );
 };
