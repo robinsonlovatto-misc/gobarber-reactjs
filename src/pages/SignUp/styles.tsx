c52a44ae3b3ddf269b32a666ae022fc00f201838
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-08-04 09:25:18 +0100

    Setting routes and animating content

--- src/pages/SignUp/styles.tsx
+++ src/pages/SignUp/styles.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 import SignUpBackground from '../../assets/sign-up-background.png';
 
@@ -11,11 +11,31 @@ export const Container = styled.div`
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-  place-content: center;
+  align-items: center;
+  justify-content: center;
   width: 100%;
   max-width: 700px;
+`;
 
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRight} 1s;
 
   form {
     margin: 80px 0;
@@ -34,13 +54,13 @@ export const Content = styled.div`
       transition: color 0.2s;
 
       &:hover {
-        color: ${shade(0.2, '#f4ede8')}
+        color: ${shade(0.2, '#f4ede8')};
       }
     }
   }
 
   > a {
-    color: #f4ede8;
+    color: #ff9000;
     display: block;
     margin-top: 24px;
     text-decoration: none;
@@ -50,14 +70,13 @@ export const Content = styled.div`
     align-items: center;
 
     &:hover {
-      color: ${shade(0.2, '#f4ede8')}
+      color: ${shade(0.2, '#ff9000')};
     }
 
     svg {
       margin-right: 16px;
     }
   }
-
 `;
 
 export const Background = styled.div`
