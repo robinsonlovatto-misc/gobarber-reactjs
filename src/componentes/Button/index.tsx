1ac621484f6a9ec800e0b394bf83a6365f360405
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2021-02-06 17:35:24 +0000

    Forgot password page

--- src/componentes/Button/index.tsx
+++ src/componentes/Button/index.tsx
@@ -1,11 +1,15 @@
 import React, { ButtonHTMLAttributes } from 'react';
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+};
 
-const Button: React.FC<ButtonProps> = ({children, ...rest}) => (
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
   // the type is required for button, but ...props will replace the button type if it is diferente
-  <Container type="button" {...rest} >{children}</Container>
+  <Container type="button" {...rest}>
+    {loading ? 'Carregando...' : children}
+  </Container>
 );
 
 export default Button;
