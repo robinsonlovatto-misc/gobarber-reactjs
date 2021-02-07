563f18af9de5f2d9b3e1f5bd00012a85610dede0
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2021-02-07 11:07:16 +0000

    Appointments listing page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import React, { useState } from 'react';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -25,6 +38,94 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://media-exp1.licdn.com/dms/image/C4E03AQGISBSK-BWuDQ/profile-displayphoto-shrink_200_200/0/1604574739445?e=1618444800&v=beta&t=ugPq2teimmewiP106ypPOd2tJwbreFXk1xTnteXEbzY"
+                alt="Robinson Lovatto"
+              />
+              <strong>Robinson Lovatto</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://media-exp1.licdn.com/dms/image/C4E03AQGISBSK-BWuDQ/profile-displayphoto-shrink_200_200/0/1604574739445?e=1618444800&v=beta&t=ugPq2teimmewiP106ypPOd2tJwbreFXk1xTnteXEbzY"
+                  alt="Robinson Lovatto"
+                />
+                <strong>Robinson Lovatto</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://media-exp1.licdn.com/dms/image/C4E03AQGISBSK-BWuDQ/profile-displayphoto-shrink_200_200/0/1604574739445?e=1618444800&v=beta&t=ugPq2teimmewiP106ypPOd2tJwbreFXk1xTnteXEbzY"
+                  alt="Robinson Lovatto"
+                />
+                <strong>Robinson Lovatto</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://media-exp1.licdn.com/dms/image/C4E03AQGISBSK-BWuDQ/profile-displayphoto-shrink_200_200/0/1604574739445?e=1618444800&v=beta&t=ugPq2teimmewiP106ypPOd2tJwbreFXk1xTnteXEbzY"
+                  alt="Robinson Lovatto"
+                />
+                <strong>Robinson Lovatto</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://media-exp1.licdn.com/dms/image/C4E03AQGISBSK-BWuDQ/profile-displayphoto-shrink_200_200/0/1604574739445?e=1618444800&v=beta&t=ugPq2teimmewiP106ypPOd2tJwbreFXk1xTnteXEbzY"
+                  alt="Robinson Lovatto"
+                />
+                <strong>Robinson Lovatto</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
